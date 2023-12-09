7b84bce3df0927b9ff8434e8470604383e4a434d
FeliOjedaRuiz <feliojedaruiz@gmail.com>
2023-12-10 00:40:15 +0100

    Add restore progress from local storage

--- src/contexts/ProgressStore.jsx
+++ src/contexts/ProgressStore.jsx
@@ -2,17 +2,27 @@ import { createContext, useCallback, useState } from "react";
 
 const ProgressContext = createContext();
 
+const restoreProgressFromLocalStorage = () => {
+  const progress = localStorage.getItem("progress");
+  if (progress) {
+    console.log(`restoring progress form local storage, progress is level ${progress}`)
+    return +progress;
+  } else {
+    return undefined;
+  }
+};
+
 function ProgressStore({ children }) {
-  const [progress, setProgress] = useState(1);
+  const [progress, setProgress] = useState(restoreProgressFromLocalStorage());
 
   const handleUnlockLevel2 = useCallback(() => {
-    console.log("Updating progress level 2");
+    console.log("Updating progress: level 2");
     setProgress(2);
     localStorage.setItem("progress", 2);
   }, []);
 
   const handleUnlockLevel3 = useCallback(() => {
-    console.log("Updating progress level 3");
+    console.log("Updating progress: level 3");
     setProgress(3);
     localStorage.setItem("progress", 3);
   }, []);
